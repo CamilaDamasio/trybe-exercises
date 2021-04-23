1b893fd0fb7af6374eb0bcffed000f6bc8bc8fab
Camila Damásio <camiladamasioef@hotmail.com>
2021-04-23 20:04:15 -0300

    Exercicio for - conteudos

---
+++ exercises/exercises_conteudos/for.js
@@ -0,0 +1,5 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let elementsList = 0; elementsList < groceryList.length; elementsList += 1) {
+    console.log(groceryList[elementsList]);
+}
