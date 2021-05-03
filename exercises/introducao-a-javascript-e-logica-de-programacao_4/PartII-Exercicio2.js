289ed9ca3fb829d89a5fdb832f94bb5b0230cbc7
Camila Damásio <camiladamasioef@hotmail.com>
2021-05-03 15:46:26 -0300

    Adiciona exercicios

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/PartII-Exercicio2.js
@@ -0,0 +1,10 @@
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
+function indiceMaiorNumero(numeros) {
+    let indiceMaior = 0;
+    for (let index in numeros) {
+        
+    }
+}
